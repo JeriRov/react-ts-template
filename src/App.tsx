bd9f49b9b53c0cff1e7ce552f3da764039d9d8b8
Artem Mahey <artemmagej@gmail.com>
2023-07-31 13:32:41 +0300

    chore: setup template

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,21 +7,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="logo" className="App-logo" src={logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          href="https://github.com/JeriRov/react-ts-template"
+          rel="noreferrer"
+          target="_blank">
+          React template with TypeScript
         </a>
       </header>
     </div>
   );
 }
 
+// eslint-disable-next-line import/no-default-export
 export default App;
